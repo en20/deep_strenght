c8f08403dce760708949decc8122f9e2785b2d75
Enzo <enzolozano90@gmail.com>
2023-09-11 15:37:56 -0300

    FRONT END PÁGINA DE LOGIN DESENVOLVIDO

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -5,22 +5,7 @@ import Image from "next/image";
 import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
-  const handleMenuClick = () => {
-    toggleMenu();
-    if (isMenuOpen) {
-      closeMenu();
-    }
-  };
 
   return (
     <nav className="flex items-center justify-between">
@@ -33,27 +18,23 @@ export default function Navbar() {
           className="my-4 mx-4 pb-2"
         />
       </Link>
-      <div className="p-4 md:hidden">
-        <AiOutlineMenu
-          className="text-2xl cursor-pointer"
-          onClick={handleMenuClick}
-        />
-      </div>
+      
+      
       <div
-        className={`md:mr-8 md:items-end md:justify-end md:text-right md:flex md:space-x-2 ${
-          isMenuOpen ? "flex flex-col" : "hidden"
-        }`}
+        className="md:mr-8 md:items-end md:justify-end md:text-right md:flex md:space-x-2 text-xs md:text-xl" 
+          
+        
       >
         <Link
           href="/"
-          onClick={closeMenu}
+          
           className="hover:text-red-500  transition-colors duration-300 ease-in-out px-4 py-2 rounded-lg font-abc"
         >
           REGISTER
         </Link>
         <Link
-          href="/"
-          onClick={closeMenu}
+          href="/Login"
+          
           className="hover:text-red-500  transition-colors duration-300 ease-in-out px-4 py-2 rounded-lg font-abc"
         >
           LOGIN
